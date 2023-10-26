851a0511f845d1b22f19293e3d33cdb12a92e43d
quocbao369 <quocbao27012002@gmail.com>
2023-10-26 08:33:40 +0700

    bao

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -12,34 +12,38 @@ class _LoginScreenState extends State<LoginScreen> {
   final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
 
-  void _login() {
-    String username = _usernameController.text;
-    String password = _passwordController.text;
+  final _formKey = GlobalKey<FormState>(); // Khởi tạo GlobalKey cho Form
 
-    // Kiểm tra thông tin đăng nhập (trong trường hợp thực, bạn sẽ gửi yêu cầu đến server để kiểm tra thông tin)
-    if (username == 'user' && password == 'password') {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => const HomeScreeen()),
-      );
-    } else {
-      showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: const Text('Đăng nhập thất bại'),
-            content: const Text('Tên người dùng hoặc mật khẩu không đúng.'),
-            actions: <Widget>[
-              TextButton(
-                child: const Text('Đóng'),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-              ),
-            ],
-          );
-        },
-      );
+  void _login() {
+    if (_formKey.currentState!.validate()) {
+      String username = _usernameController.text;
+      String password = _passwordController.text;
+
+      // Kiểm tra thông tin đăng nhập
+      if (username == 'admin' && password == '1') {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => const HomeScreeen()),
+        );
+      } else {
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: const Text('Đăng nhập thất bại'),
+              content: const Text('Tên người dùng hoặc mật khẩu không đúng.'),
+              actions: <Widget>[
+                TextButton(
+                  child: const Text('Đóng'),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            );
+          },
+        );
+      }
     }
   }
 
@@ -48,39 +52,54 @@ class _LoginScreenState extends State<LoginScreen> {
     return Scaffold(
       body: Center(
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 500),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text(
-                "Đăng nhập",
-                style: TextStyle(
-                  fontSize: 40,
-                  fontWeight: FontWeight.bold,
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Form(
+            key: _formKey, // Gán GlobalKey cho Form
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text(
+                  "Đăng nhập",
+                  style: TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-              TextField(
-                controller: _usernameController,
-                decoration: const InputDecoration(
-                  hintText: 'Nhập tên người dùng',
-                  labelText: 'Tên người dùng',
+                TextFormField(
+                  controller: _usernameController,
+                  decoration: const InputDecoration(
+                    hintText: 'Nhập tên người dùng',
+                    labelText: 'Tên người dùng',
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Vui lòng nhập tên người dùng';
+                    }
+                    return null;
+                  },
                 ),
-              ),
-              const SizedBox(height: 20),
-              TextField(
-                controller: _passwordController,
-                obscureText: true,
-                decoration: const InputDecoration(
-                  hintText: 'Nhập mật khẩu',
-                  labelText: 'Mật khẩu',
+                const SizedBox(height: 20),
+                TextFormField(
+                  controller: _passwordController,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    hintText: 'Nhập mật khẩu',
+                    labelText: 'Mật khẩu',
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Vui lòng nhập mật khẩu';
+                    }
+                    return null;
+                  },
                 ),
-              ),
-              const SizedBox(height: 30),
-              ElevatedButton(
-                onPressed: _login,
-                child: const Text('Đăng nhập'),
-              ),
-            ],
+                const SizedBox(height: 30),
+                ElevatedButton(
+                  onPressed: _login,
+                  child: const Text('Đăng nhập'),
+                ),
+              ],
+            ),
           ),
         ),
       ),
